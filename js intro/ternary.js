b03a3f69f31875783da57e218a4f045d35fe8a93
tuhinXtg <natuhin02@gmail.com>
2026-05-02 00:41:23 +0600

    ternary

---
+++ js intro/ternary.js
@@ -0,0 +1,11 @@
+// ternary
+
+const age = 20;
+// if(age >= 18){
+//     console.log('Yes you can')
+// }
+// else{
+//     console.log('No, You can not')
+// }
+
+age >= 18 ? console.log('vote dio') : console.log('Vote dio na')
