92b0ca1a0c95c8d3e82e4d13a45801af5bcd6ac6
tuhinXtg <natuhin02@gmail.com>
2026-05-02 12:02:23 +0600

    true

---
+++ js intro/shortcut.js
@@ -0,0 +1,15 @@
+// const isleader = true;
+// if(isleader){
+//     console.log('leader')
+// }
+// else{
+//     console.log('No')
+// }
+
+const ispassed = false;
+if(ispassed){
+    console.log('OK')
+}
+else{
+    console.log('NO')
+} 
