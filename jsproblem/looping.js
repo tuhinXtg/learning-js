17f5d46dc154283546ede8d4d6929495b4d24d14
tuhinXtg <natuhin02@gmail.com>
2026-05-05 16:02:01 +0600

    traversal

---
+++ jsproblem/looping.js
@@ -0,0 +1,11 @@
+const friends = ['tuhin', 'mahim', 'jahid', 'nahid'];
+
+for (const frnd of friends){
+    // console.log(frnd)
+}
+
+
+for(let i = 0; i<friends.length; i++){
+    console.log(i);
+    console.log(friends[i]);
+}
