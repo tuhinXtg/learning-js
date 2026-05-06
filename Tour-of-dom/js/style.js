bcba2c11cb295def3e0fc83ad0f71e44b651a8c4
tuhinXtg <natuhin02@gmail.com>
2026-05-06 16:50:35 +0600

    style with js

---
+++ Tour-of-dom/js/style.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll('section')
+// console.log(sections);
+for(const section of sections){
+    section.style.border = '3px solid blue';
+    // section.style.gap = ''
+}
+
+
+
+
+
+
+
