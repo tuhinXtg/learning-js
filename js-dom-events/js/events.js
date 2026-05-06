7de558752ad350e6be3f227f71d118b49bededc9
tuhinXtg <natuhin02@gmail.com>
2026-05-06 20:17:41 +0600

    made buttons with js to change body color

--- js-dom-events/js/events.js
+++ js-dom-events/js/events.js
@@ -11,3 +11,23 @@ makebluebtn.onclick = makeble;
 function makeble() {
     document.body.style.backgroundColor = 'blue';
 }
+
+// option 4
+const pinkbutton = document.getElementById('make-pink')
+pinkbutton.addEventListener('click', makepink)
+
+function makepink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// another option 4
+const makegreen = document.getElementById('make-green')
+makegreen.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+
+// option 4: Final version 
+// Important approach 
+document.getElementById('make-golden').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenrod'
+})
