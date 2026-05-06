f5c7837dcbe343d28cbfe876b20e7f0eb781db4a
tuhinXtg <natuhin02@gmail.com>
2026-05-06 19:00:16 +0600

    button events with js

--- js-dom-events/js/events.js
+++ js-dom-events/js/events.js
@@ -1,1 +1,13 @@
 console.log('This is separate js file')
+
+//add onclick function
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// option 3:  
+const makebluebtn = document.getElementById('make-blue');
+makebluebtn.onclick = makeble;
+function makeble() {
+    document.body.style.backgroundColor = 'blue';
+}
