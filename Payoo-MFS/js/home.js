8687a4b4c29f9b0cf99a7e8e38da1b5786c5b66b
tuhinXtg <natuhin02@gmail.com>
2026-05-10 13:42:43 +0600

    add money completed

--- Payoo-MFS/js/home.js
+++ Payoo-MFS/js/home.js
@@ -7,5 +7,17 @@ document.getElementById('add-money-btn')
         event.preventDefault();
         const get_amount = document.getElementById('amount-input').value;
         const pin_number = document.getElementById('pin-number').value;
-        
+
+        if (pin_number == '111') {
+            const balance = document.getElementById('acc-balance').innerText;
+
+            const add_money_number = parseFloat(get_amount);
+            const balance_number = parseFloat(balance);
+
+            const new_balance = add_money_number + balance_number;
+
+            document.getElementById('acc-balance').innerText = new_balance;
+        } else {
+            alert('Failed to add money. Tyr again please.')
+        }
     })
