522b91a3703885d292da02ccd4eacd310e168d43
tuhinXtg <natuhin02@gmail.com>
2026-05-10 13:24:57 +0600

    add money in porgress

--- Payoo-MFS/js/home.js
+++ Payoo-MFS/js/home.js
@@ -1,1 +1,11 @@
-console.log('Home added')
+// console.log('Home added')
+
+// add an event handler method tho the add money button..........
+document.getElementById('add-money-btn')
+    .addEventListener('click', function (event) {
+        // prevent page reload after submit...........
+        event.preventDefault();
+        const get_amount = document.getElementById('amount-input').value;
+        const pin_number = document.getElementById('pin-number').value;
+        
+    })
