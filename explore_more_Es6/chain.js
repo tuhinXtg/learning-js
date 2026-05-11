0e39f153f80565379546872ac5669a43ac328bec
tuhinXtg <natuhin02@gmail.com>
2026-05-11 11:17:21 +0600

    data chain validation

---
+++ explore_more_Es6/chain.js
@@ -0,0 +1,16 @@
+// Data access
+
+// const data = [{ id: 1, name: 'Tuhin', address: 'kochu khet' }];
+
+// console.log(data[0].name)
+
+
+const products = {
+    count : 5000,
+    data:[
+            { id: 1, name: 'Tuhin', price: 3000 },
+            { id: 2, name: 'Rahim', price: 4000},
+    ]
+}
+
+console.log(products.data[1].price)
---
+++ explore_more_Es6/recap.js
@@ -0,0 +1,14 @@
+const a = 50;
+const arr = [2,3,4,5]
+const person = {
+    name: 'Tuhin',
+    age: 23
+}
+const messege = `The boy was ${person.age} years old`
+
+if(true){
+    console.log(messege)
+}
+
+const square = x => x*X;
+const sum = (a,b) => a+b;
