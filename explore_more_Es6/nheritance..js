d376f6ceec0d88320e7b9dafffe4c92bc150a7a6
tuhinXtg <natuhin02@gmail.com>
2026-05-11 19:11:27 +0600

    inheritance

---
+++ explore_more_Es6/nheritance..js
@@ -0,0 +1,25 @@
+class Vehicle {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    move() {
+        console.log('Vehicle is not working.')
+    }
+}
+
+class Bus extends Vehicle {
+    constructor(name, price, seat, ticketPrice) {
+        super(name, price);
+        this.seat = seat;
+        this.ticketPrice = ticketPrice;
+    }
+
+}
+
+class Truck extends Vehicle{
+    constructor(name,price,load){
+        super(name,price);
+        this.load = load;
+    }
+}
