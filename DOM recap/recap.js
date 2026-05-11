39174fa23d3f872343e13270fa690d48bfceec12
tuhinXtg <natuhin02@gmail.com>
2026-05-11 08:30:41 +0600

    login

---
+++ DOM recap/recap.js
@@ -0,0 +1,15 @@
+// how to use javascript with hyml and css
+// get elements or elements from dom
+//     getelementbyId
+//     getelementbyclassname
+//     queryselectora
+//     queryselectorall
+//
+// dynamically add element to the DOM
+// innner html set 
+// usre innner=text (h1,p,any other)
+// use value for (input fields, textarea)
+// use event listner 
+document.querySelectorAll('.users-container .disable h3')
+document.querySelectorAll('.my-users li')
+// use function to reduce duplicate codes
