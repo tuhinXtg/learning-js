b39a6a8fa9817a3263ea0d894f36a0ae17e878ad
tuhinXtg <natuhin02@gmail.com>
2026-05-11 10:05:16 +0600

    destructor

---
+++ ES6 Intro/destructuin.js
@@ -0,0 +1,18 @@
+const actor = {
+    name: 'anata',
+    age: 30,
+    phone:'223324',
+    money: 73479732
+}
+// 
+const {age} = actor
+
+// const age = actor.age;
+// const money = actor.money;
+
+console.log(actor)
+console.log(actor)
+console.log(actor)
+// console.log(money)
+// console.log(money)
+// console.log(money)
