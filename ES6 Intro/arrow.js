8cfa8ec7ecd3b38d4493e4127944b508df1ee8e3
tuhinXtg <natuhin02@gmail.com>
2026-05-11 09:43:25 +0600

    login

---
+++ ES6 Intro/arrow.js
@@ -0,0 +1,19 @@
+// function add(a,b){
+//     const result = a + b;
+//     return result;
+// }
+
+// const sum = add(5,7);
+// console.log(sum);
+
+
+
+// // function expression
+// const add2 = function(){
+//     return a + b;
+// };
+
+// arrow function
+const add3 = (a,b) => a+b;
+const sum = add3(5,5);
+console.log(sum);
---
+++ ES6 Intro/moreArrow.js
@@ -0,0 +1,6 @@
+const difference = (x,y) => x-y;
+const multy = (first ,second ,third) => first*second*third;
+const getage = (person) => person.age;
+const student = {name: 'anata', age: 45}
+const age = getage(student)
+console.log(age) 
