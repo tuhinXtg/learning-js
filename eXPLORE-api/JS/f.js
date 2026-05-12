5d64de6803005d9ecd30395efb0a0b8b923ba7cb
tuhinXtg <natuhin02@gmail.com>
2026-05-12 16:10:32 +0600

    More about Fetch

---
+++ eXPLORE-api/JS/f.js
@@ -0,0 +1,17 @@
+const { jsx } = require("react/jsx-runtime");
+
+function loadData2() {
+    fetch('https:jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => displayUsers(data))
+}
+
+function displayUsers(data){
+    console.log(data);
+}
