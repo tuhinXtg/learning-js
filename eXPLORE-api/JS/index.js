3dc8ce21c497e85f8b1e3e5319522f41d5149b3c
tuhinXtg <natuhin02@gmail.com>
2026-05-12 16:34:18 +0600

    fetch data and showcase the information in UI

---
+++ eXPLORE-api/JS/index.js
@@ -0,0 +1,15 @@
+const { createElement, use } = require("react")
+
+function loadUsers2(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then( res => res.json())
+    .then( data => displayData(data))
+}
+function displayData(data){
+    const ul = document.getElementById('user-list');
+    for(const user of data){
+        const li = document.createElement('li')
+        li.innerText = user.id;
+        ul.appendChild(li);
+    }
+}
