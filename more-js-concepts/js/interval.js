8cc2d465e4735b0a7c26ba1e6418fdb0ecba783f
tuhinXtg <natuhin02@gmail.com>
2026-05-13 10:44:37 +0600

    settimeout and setinterval

---
+++ more-js-concepts/js/interval.js
@@ -0,0 +1,17 @@
+console.log(1);
+console.log(2);
+// setTimeout(() => {
+//     console.log(3)
+// },);
+// setTimeout(() => {
+//     console.log(3)
+// }, 4000);
+console.log(4);
+console.log(5);
+console.log(6);
+
+let num = 0;
+const clockId = setInterval(() => {
+    num++;
+    console.log(clockId, num)
+}, 2000);
