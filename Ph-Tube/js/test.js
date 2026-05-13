c636ae3224b636f2306ad271cb7553832fcc097e
tuhinXtg <natuhin02@gmail.com>
2026-05-13 18:45:55 +0600

    videos with all informations has pushed from jason ro UI

---
+++ Ph-Tube/js/test.js
@@ -0,0 +1,10 @@
+// function getTimeString(time){
+//     // converting in hour''
+//     const hour = time / 3600;
+//     const remainingMinute = time % 3600;
+//     // converting in minute..
+//     const minute = remainingMinute / 60;
+//     const second = remainingMinute % 60;
+
+//     return `${hour}:${minute}:${second}`
+// }
--- Ph-Tube/js/video.js
+++ Ph-Tube/js/video.js
@@ -1,3 +1,16 @@
+function getTimeString(time){
+    // converting in hour''
+    const hour = parseInt(time / 3600);
+    const remainingMinute = time % 3600;
+    // converting in minute..
+    const minute = parseInt(remainingMinute / 60);
+    const second = remainingMinute % 60;
+
+    return `${hour}:${minute}:${second}`
+}
+
+
+// button work start
 const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
         .then(res => res.json())
@@ -17,4 +30,51 @@ const displayCategories = (data) => {
     });
 }
 
+// button work end
+
+
+// videos work start
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch(err => console.log(err))
+}
+
+
+const displayVideos = (videos) => {
+    const video_container = document.getElementById('videos')
+    videos.forEach(element => {
+        const card = document.createElement('div');
+        card.classList = "card bg-base-100 shadow-sm"
+        card.innerHTML = `
+             <figure class="h-[200px] relative">
+                <img
+                src=${element.thumbnail}
+                alt="videos" class="h-full w-full object-cover" />
+                ${
+                    element.others.posted_date ? `<span class="absolute right-2 bottom-2 bg-black text-white">${getTimeString(element.others.posted_date)}</span>` : ""
+                }
+            </figure>
+            <div class="px-0 py-4 flex flex-row justify-between">
+                <div class = "p-2">
+                    <img class="rounded-full w-[70px] h-[70px] object-cover"
+                    src=${element.authors[0].profile_picture}
+                    alt="videos" class="h-full w-full object-cover" /> 
+                </div>
+                <div class="w-9/12 justify-between py-2">
+                    <h2 class="card-title pb-3 font-semibold">${element.title}</h2>
+                    <p class = "text-xs">${element.authors[0].profile_name}</p>
+                    <p class = "text-xs">${element.others.views} views</p>
+                </div>
+            </div>
+        `
+        video_container.appendChild(card);
+    });
+}
+
+// videos work end
+
+// called functions
 loadCategories()
+loadVideos()
