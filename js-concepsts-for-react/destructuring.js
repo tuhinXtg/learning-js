2d9641b41d6f5b46f33958837cf0111fd21dc0f3
tuhinXtg <natuhin02@gmail.com>
2026-05-14 17:53:09 +0600

    array and object destructuring

---
+++ js-concepsts-for-react/destructuring.js
@@ -0,0 +1,17 @@
+const numbers = [1,2,3,4,5,6,7]
+// const x = numbers [0]
+// const y = numbers [1]
+const [x,y] = [34,56]
+console.log(x,y)
+
+
+// object destructuring
+const {name,age}= {
+    name: 'alu',
+    age: 14
+}
+const {name,age}= {
+    id: 12,
+    name: 'alu',
+    age: 14
+}
