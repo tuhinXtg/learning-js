e39fc264f5c2044523a48e0ab4ef9cec6c3ca6d9
tuhinXtg <natuhin02@gmail.com>
2026-05-14 18:16:02 +0600

    ternary

---
+++ js-concepsts-for-react/shortcut.js
@@ -0,0 +1,12 @@
+let money = 80;
+// let food;
+// if(mpney > 100){
+//     food = 'biriyani'
+// }else{
+//     food = 'cha'
+// }
+
+// /ternary
+
+let food1 = money > 100 ? 'biriyani' : 'cha';
+console.log(food1)
