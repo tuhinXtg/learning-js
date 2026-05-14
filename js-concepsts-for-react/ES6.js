a11e3c081a7b07bdd464e65c42337aaca93f529b
tuhinXtg <natuhin02@gmail.com>
2026-05-14 17:38:19 +0600

    js fundamentals for react

---
+++ js-concepsts-for-react/ES6.js
@@ -0,0 +1,10 @@
+const number = [1,2,3,4,5]
+const student = {
+    name: 'Nazmul',
+    age: 32,
+    movies: ['hte', 'isdnf', 'sojdf']
+
+}
+
+const about = `my name is ${student.name}`
+console.log(about) 
---
+++ js-concepsts-for-react/fundamental.js
@@ -0,0 +1,10 @@
+// how to declare a variable using let and const
+// 6 basic conditions >,<, === ,!== ,<=,>=
+// multiple conditions:  &&, || 
+// array
+// length, push, for loop
+// functions
+// object 
+// ways to access property by name
+
+
