513c28fccb66ae76d07f2636761a6315c07a4376
tuhinXtg <natuhin02@gmail.com>
2026-05-17 15:16:44 +0600

    dot notation

---
+++ js-concepsts-for-react/notation.js
@@ -0,0 +1,15 @@
+const person = {
+    name: 'Nazmul',
+    prop: 'Traffic surgeon',
+    age: 34,
+    address: 'Shahbag',
+}
+
+// dot notation
+
+const profession = person.prop;
+
+// bracet notation
+
+const profession2 = person['prop']
+console.log(profession2)
