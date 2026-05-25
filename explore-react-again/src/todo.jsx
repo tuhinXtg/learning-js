c1488874ec0f4a2a05c33ff89dfbaefbbdd2762b
tuhinXtg <natuhin02@gmail.com>
2026-05-25 17:52:24 +0600

    ternery operators

--- explore-react-again/src/todo.jsx
+++ explore-react-again/src/todo.jsx
@@ -1,10 +1,25 @@
+// export default function Todo({task, isdone}){
+//     if(isdone === true){
+//         return(
+//             <li>Task : {task}</li>
+//         )
+//     }
+//     else{
+//         return <li>Work on : {task}</li>
+//     }
+// }
+
+
+// conditional rendering 3 ternary operator
+// export default function Todo({task, isdone}){
+//     return(
+//         <li>{isdone ? 'Finished' : 'Work on'} : {task}</li>
+//     )
+// }
+
+// conditional rendering option 4: 
 export default function Todo({task, isdone}){
-    if(isdone === true){
-        return(
-            <li>Task : {task}</li>
-        )
-    }
-    else{
-        return <li>Work on : {task}</li>
-    }
+    return(
+        <li>{task} {isdone && ':Done'}</li>
+    )
 }
