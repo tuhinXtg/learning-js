cf24e8eb11580531ccc5a2c7315d2eb209b83b2a
tuhinXtg <natuhin02@gmail.com>
2026-05-30 17:02:32 +0600

    initializing new practice project knowledge-cafe

---
+++ react-knowledge-cafe/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-xl bg-red-300'>Knowledge cafe</h1>
+    </div>
+  )
+}
+
+export default App
