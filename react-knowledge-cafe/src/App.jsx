658fab6efb48a683bf99a0d32b9a58b7c339d8b9
tuhinXtg <natuhin02@gmail.com>
2026-05-30 17:23:30 +0600

    fetching fake data

--- react-knowledge-cafe/src/App.jsx
+++ react-knowledge-cafe/src/App.jsx
@@ -1,14 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
 import './App.css'
+import Blogs from './components/Blogs/blogs'
+import Header from './components/header/header'
 
 function App() {
 
   return (
     <div>
-      <h1 className='text-xl bg-red-300'>Knowledge cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </div>
   )
 }
---
+++ react-knowledge-cafe/src/components/header/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-xl font-bold'>Knowledge cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
