527a081e264c1bb1b87bb82c407b731de4e4b5fc
tuhinXtg <natuhin02@gmail.com>
2026-05-30 11:48:57 +0600

    recap

--- react-world-tour/src/Components/Country/country.jsx
+++ react-world-tour/src/Components/Country/country.jsx
@@ -1,9 +1,11 @@
 const Country = ({country}) => {
-    const {name} = country;
+    const {name, area, population} = country;
     return (
         <div className="country">
             <h3>Name : {name.common}</h3>
             <img src={country.flags.png} alt="" />
+            <p>Population : {population}</p>
+            <p>Area : {area}</p>
         </div>
     );
 };
